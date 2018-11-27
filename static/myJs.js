7189f53df06b3398608bfb4b3a9934cf9c9e7741
bakatarekun <ckyamanaka@gmail.com>
2018-11-26 21:38:09 -0500

    add staff hours

--- static/myJs.js
+++ static/myJs.js
@@ -1,111 +1,116 @@
- function ajaxcall() {
+function ajaxcall() {
 
 
-     temp = "/topics/" + temp;
-     $.ajax({
-         type: "GET",
-         //url: "/topics/2",
-         url: temp,
-         // data: Course,
-         dataType: "json",
-         contentType: "application/json",
-         success: function (data) {
+    temp = "/topics/" + temp;
+    $.ajax({
+        type: "GET",
+        //url: "/topics/2",
+        url: temp,
+        // data: Course,
+        dataType: "json",
+        contentType: "application/json",
+        success: function (data) {
 
-             var str = data.id + " " + data.name + " " + data.description;
+            var str = data.id + " " + data.name + " " + data.description;
 
-             $("#mydiv").text(str);
-
-
-         },
-
-         error: function () {
-
-             alert("wasn't succesful");
-
-         }
-     });
- }
+            $("#mydiv").text(str);
 
 
+        },
 
- function togglecells2(that){
-     alert("function fired")
- }
+        error: function () {
 
-  // function togglecells(targetcell,classname) {
-  //
-  //               var isMouseDown = false;
-  //
-  //               $('[name=tb]')
-  //                   .on('mousedown', targetcell, function() {
-  //                       isMouseDown = true;
-  //                       $(this).toggleClass(classname);
-  //                   })
-  //                   .on('mouseover', targetcell, function() {
-  //                       if (isMouseDown)
-  //                           $(this).addClass(classname);
-  //                   })
-  //                   .on('mouseup', targetcell, function() {
-  //                       isMouseDown = false;
-  //                   })
-  //                   .on('mouseleave', function() {
-  //                       isMouseDown = false;
-  //                   });
-  //
-  //              $(targetcell).click( function() {
-  //                   if (isMouseDown==false)
-  //                       if($(this).hasClass(classname))
-  //                       {      $(this).removeClass(classname);   }
-  //                       else {
-  //                            $(this).addClass(classname);
-  //                       }
-  //               })
-  //           }
+            alert("wasn't succesful");
 
- function togglecells(targetcell,classname) {
+        }
+    });
+}
 
-                var isMouseDown = false;
-                var count =25;
-                   $('#hoursleft').text(count);
-                 $('[name=tb]')
-                    .on('mousedown', targetcell, function() {
-                        isMouseDown = true;
-                         count=count-1;
-                        if ($(this).hasClass(classname)) {
-                                 $(this).removeClass(classname);
 
-                             }
-                             else
-                            $(this).addClass(classname);
-                    })
-                     .on('mouseup', targetcell, function() {
-                        isMouseDown = false;
-                    })
-                    .on('mouseover', targetcell, function() {
 
-                        if (isMouseDown)
-                        {
-                             if ($(this).hasClass(classname)) {
-                                 $(this).removeClass(classname);
-                               //  $('#hoursleft').text(count);
+function togglecells2(that){
+    alert("function fired")
+}
 
-                             }
-                             else
-                            $(this).addClass(classname);
-                        }
-                    })
-                    .on('mouseleave', function() {
-                        if (isMouseDown) {
+// function togglecells(targetcell,classname) {
+//
+//               var isMouseDown = false;
+//
+//               $('[name=tb]')
+//                   .on('mousedown', targetcell, function() {
+//                       isMouseDown = true;
+//                       $(this).toggleClass(classname);
+//                   })
+//                   .on('mouseover', targetcell, function() {
+//                       if (isMouseDown)
+//                           $(this).addClass(classname);
+//                   })
+//                   .on('mouseup', targetcell, function() {
+//                       isMouseDown = false;
+//                   })
+//                   .on('mouseleave', function() {
+//                       isMouseDown = false;
+//                   });
+//
+//              $(targetcell).click( function() {
+//                   if (isMouseDown==false)
+//                       if($(this).hasClass(classname))
+//                       {      $(this).removeClass(classname);   }
+//                       else {
+//                            $(this).addClass(classname);
+//                       }
+//               })
+//           }
 
-                                 $(this).removeClass(classname);
+function togglecells(targetcell,classname) {
 
-                         }
-                        isMouseDown = false;
-                    })
-                   ;
+    var isMouseDown = false;
+    var count =25;
+    $('#hoursleft').text(count);
+    $('[name=tb]')
+        .on('mousedown', targetcell, function() {
+            isMouseDown = true;
+            count=count-1;
 
+            if ($(this).hasClass(classname)) {
+                $(this).removeClass(classname);
 
             }
+            else
+            {
+                $(this).addClass(classname);
+
+            }
+        })
+        .on('mouseup', targetcell, function() {
+            isMouseDown = false;
+        })
+        .on('mouseover', targetcell, function() {
+
+            if (isMouseDown)
+            {
+
+                if ($(this).hasClass(classname)) {
+                    $(this).removeClass(classname);
+                }
+                else
+                {
+                    $(this).addClass(classname);
+                }
+            }
+        })
+        .on('mouseleave', function() {
+            if (isMouseDown) {
+
+                $(this).removeClass(classname);
+
+            }
+            isMouseDown = false;
+        })
+    ;
+
+
+}
 
 
 
