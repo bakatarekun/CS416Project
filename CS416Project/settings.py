8abbac66f92925cfbe21447b61944c8e7eefea86
bakatarekun <35007025+bakatarekun@users.noreply.github.com>
2019-07-13 07:36:10 -0400

    Update settings.py
    Changed Staticfiles_Dirs to the apporpriate location for the new URL

--- CS416Project/settings.py
+++ CS416Project/settings.py
@@ -128,7 +128,7 @@ LOGOUT_REDIRECT_URL = '/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     # uncomment when use on pythonanywhere
-    #'/home/Bakatarekun2/bakatarekun2.pythonanywhere.com/static',
+    '/home/Majikayo/Majikayo.pythonanywhere.com/static',
     #'/var/www/static/',
 ]
 
