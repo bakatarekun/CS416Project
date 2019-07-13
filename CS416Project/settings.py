14db5a7e276f65268e47c86070f3e330f8dc0aa3
bakatarekun <35007025+bakatarekun@users.noreply.github.com>
2019-07-13 08:37:23 -0400

    Update settings.py

--- CS416Project/settings.py
+++ CS416Project/settings.py
@@ -126,7 +126,7 @@ LOGOUT_REDIRECT_URL = '/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    #os.path.join(BASE_DIR, "static"),
     # uncomment when use on pythonanywhere
     '/home/Majikayo/majikayo.pythonanywhere.com/static',
     #'/var/www/static/',
